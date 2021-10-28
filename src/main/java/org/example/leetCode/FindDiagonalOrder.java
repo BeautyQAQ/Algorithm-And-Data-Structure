ce3025276f9d9117d094b4f1562789a45755a9a8
huangshen <1426887150@qq.com>
2021-10-28 20:44:32 +0800

    No.73 零矩阵

---
+++ src/main/java/org/example/leetCode/FindDiagonalOrder.java
@@ -0,0 +1,11 @@
+package org.example.leetCode;
+
+/**
+ * No.498 对角线遍历
+ */
+public class FindDiagonalOrder {
+    public int[] findDiagonalOrder(int[][] mat) {
+        
+        return null;
+    }
+}
---
+++ src/main/java/org/example/leetCode/SetZeroes.java
@@ -0,0 +1,30 @@
+package org.example.leetCode;
+
+/**
+ * No.73 零矩阵
+ */
+public class SetZeroes {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean[] rows = new boolean[row];
+        boolean[] cols = new boolean[col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // 记录需要变0的行和列
+                if (matrix[i][j]==0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
