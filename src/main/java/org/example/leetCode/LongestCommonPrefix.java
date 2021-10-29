225c45634c996d4012ba007dc25b2680e28e60f4
huang丶shen <1426887150@qq.com>
2021-10-29 15:16:21 +0800

    No.14 最长公共前缀 纵向扫描

--- src/main/java/org/example/leetCode/LongestCommonPrefix.java
+++ src/main/java/org/example/leetCode/LongestCommonPrefix.java
@@ -4,6 +4,12 @@ package org.example.leetCode;
  * No.14 最长公共前缀
  */
 public class LongestCommonPrefix{
+    
+    /**
+     * 横向扫描
+     * @param strs strs
+     * @return String
+     */
     public String longestCommonPrefix(String[] strs) {
         if(strs==null||strs.length==0){
             return "";
@@ -29,5 +35,26 @@ public class LongestCommonPrefix{
             index++;
         }
         return str1.substring(0,index);
+    }
+    
+    /**
+     * 纵向扫描
+     * @param strs strs
+     * @return String
+     */
+    public String longestCommonPrefix1(String[] strs) {
+        if (strs==null||strs.length==0){
+            return "";
+        }
+        // 纵向扫描: 遍历第一个字符串的字符,并与其余字符串响应的位置比较
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                if (i==strs[j].length()||strs[j].charAt(i)!=c){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
     }
 }
