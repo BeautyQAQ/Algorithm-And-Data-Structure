c7cfebe2da04d3dc4c13520d5e2608d7e6f22582
huangshen <1426887150@qq.com>
2021-10-30 23:06:22 +0800

    No.344 反转字符串 No.28 strStr() KMP算法

---
+++ src/main/java/org/example/leetCode/ReverseString.java
@@ -0,0 +1,22 @@
+package org.example.leetCode;
+
+/**
+ * No.344 反转字符串
+ */
+public class ReverseString {
+    public void reverseString(char[] s) {
+        if (s.length<=1) {
+            return;
+        }
+        int left = 0, right = s.length-1;
+        char c;
+        // 左右位置互换
+        while (left<right) {
+            c = s[left];
+            s[left] = s[right];
+            s[right] = c;
+            left++;
+            right--;
+        }
+    }
+}
