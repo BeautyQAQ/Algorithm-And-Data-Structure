8c73cb15f1c67e25904942daef7201493f3711b0
huangshen <1426887150@qq.com>
2021-11-03 23:50:28 +0800

    数组和字符串

---
+++ src/main/java/org/example/leetCode/FindMin.java
@@ -0,0 +1,21 @@
+package org.example.leetCode;
+
+/**
+ * No.153 寻找旋转排序数组中的最小值
+ * 
+ * 二分查找
+ */
+public class FindMin {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length-1;
+        while (left < right){
+            int mid = left + (right-left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
