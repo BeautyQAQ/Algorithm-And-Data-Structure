1d58037bb46d14a0541d3520d90fa316b3cf76d4
huangshen <1426887150@qq.com>
2021-12-14 23:31:56 +0800

    No.203 移除链表元素

--- src/main/java/org/example/common/ListNode.java
+++ src/main/java/org/example/common/ListNode.java
@@ -9,4 +9,34 @@ public class ListNode {
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    /**
+     * 参数为数组的构造函数
+     * @param arr 数组
+     */
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length ==0) {
+            throw new IllegalArgumentException("arr can not be empty!");
+        }
+        // 递归终止条件
+        this.val = arr[0];
+        ListNode cur = this;
+        // 循环中递归调用
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        while (cur != null) {
+            res.append(cur.val).append("->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
 }
---
+++ src/main/java/org/example/linked_list/RemoveElements.java
@@ -0,0 +1,69 @@
+package org.example.linked_list;
+
+import org.example.common.ListNode;
+
+/**
+ * No.203 移除链表元素
+ * 给你一个链表的头节点 head 和一个整数 val ，删除链表中所有满足 Node.val == val 的节点，
+ * 并返回新的头节点
+ */
+public class RemoveElements {
+    
+    public ListNode removeElements(ListNode head, int val){
+        // 先判断头节点是否是需要删除的节点
+        while (head!=null && head.val ==val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+        // 如果链表已经被删除完, 则直接返回空
+        if (head == null) {
+            return null;
+        }
+        // 删除中间的节点
+        ListNode prev = head;
+        while (prev.next != null) {
+            if(prev.next.val == val){
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            }else{
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 虚拟头节点方案
+     * @param head 链表头
+     * @param val 被删除的值
+     * @return 返回表头
+     */
+    public ListNode removeElementsDummy(ListNode head, int val){
+        // 创建虚拟头节点指向当前链表, 这样就可以不对头节点进行特殊处理
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        // 删除中间的节点
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if(prev.next.val == val){
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            }else{
+                prev = prev.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        int[] numbs = {1,2,6,3,4,5,6};
+        ListNode head = new ListNode(numbs);
+        System.out.println(head);
+        RemoveElements removeElements = new RemoveElements();
+        ListNode result = removeElements.removeElements(head, 6);
+        System.out.println(result);
+    }
+}
