5fb65fec49ad09acbc4de90837ef01f128c59cc4
huangshen <1426887150@qq.com>
2023-06-01 10:24:40 +0800

    1

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -17,4 +17,25 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    /**
+     * 冒泡排序
+     */
+    @Test
+    public void bubbleShort()
+    {
+        int[] a = {3,4,2,7,6,89,46,67,88,90,57,43};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length-1-i; j++) {
+                if (a[j]>a[j+1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
 }
